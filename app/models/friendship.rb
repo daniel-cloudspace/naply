2072530db16cfe2a8d727d35ea2331a534bbf3de
Daniel <daniel@cloudspace.com>
2011-07-17 05:45:09 -0400

    how do i make it know that user_1_id points to the users table

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,8 @@
 class Friendship < ActiveRecord::Base
     validates_uniqueness_of :user_2_id, :scope => :user_1_id
 
-    has_one :user_1, :source => :users
-    has_one :user_2, :source => :users
+    has_one :user_1, :as => :user
+    has_one :user_2, :as => :user
 
     after_create :create_conjugate_friendship
     after_destroy :destroy_conjugate_friendship
