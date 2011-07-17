e10c852a536041d5cfa9f840d6599bbc3b36eb46
Daniel <daniel@cloudspace.com>
2011-07-17 05:32:41 -0400

    friendships should have two people, aww

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,9 @@
 class Friendship < ActiveRecord::Base
     validates_uniqueness_of :user_2_id, :scope => :user_1_id
 
+    has_one :user_1
+    has_one :user_2
+
     after_create :create_conjugate_friendship
     after_destroy :destroy_conjugate_friendship
 
@@ -13,4 +16,7 @@ class Friendship < ActiveRecord::Base
         conjugate_friendship = Friendship.find_by_user_1_id_and_user_2_id(self.user_2_id, self.user_1_id)
         conjugate_friendship.destroy
     end
+
+
+    
 end
