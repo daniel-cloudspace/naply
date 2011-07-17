8a16cf7e393cbf4dc67b77c8b2196cc5cc654777
Daniel <daniel@cloudspace.com>
2011-07-16 23:55:11 -0400

    first commit, new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Naply::Application.config.secret_token = 'd23e83a89f98ada79b75f614ca9c383feef7c9f45c3c28c4202a7257735974367f56f5e519e493e406822ec8f3735d208e05022435d574238327771efd1eb833'
